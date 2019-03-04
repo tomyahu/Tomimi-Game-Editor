55937c821c4aeed21bb2bb13199116b1f4418e1c
tomyahu <tomyahu@gmail.com>
2019-03-04 12:42:40 -0300

    Updated consts to add inital app

--- main.lua
+++ main.lua
@@ -1,14 +1,15 @@
 require "Global.consts"
 require "Global.application.application"
+---------------------------------------------------------------------------
 
-----------------------------------------------------------------------------------
--- import your initial app here
-local initial_app = require "Menu._init"
-----------------------------------------------------------------------------------
-----------------------------------------------------------------------------------
--- Register your Apps here
--- Apps Registered
-----------------------------------------------------------------------------------
+local initial_app = require( APPS[INITIAL_APP] )
+
+for appName, appInit in pairs(APPS) do
+    application:registerApp(appName, appInit)
+end
+
+application:setCtrl(initial_app["ctrl"])
+application:setView(initial_app["view"])
 
 function love.load()
     application:setLocalContext(CurrentView:getContextVars())
