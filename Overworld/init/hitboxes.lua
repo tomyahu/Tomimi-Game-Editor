c9ae2f90de522832c07e9a08d6013edb67359c10
tomyahu <tomyahu@gmail.com>
2019-03-07 15:15:06 -0300

    Updated hitboxes

--- Overworld/init/hitboxes.lua
+++ Overworld/init/hitboxes.lua
@@ -2,11 +2,11 @@ local RectangleHitbox = require "lib.physics.hitboxes.RectangleHitbox"
 --------------------------------------------------------------------------------------------------------
 
 function newPlayerHitboxes()
-    return {RectangleHitbox.new(8, 8, 24, 24)}
+    return {RectangleHitbox.new(16, 40, 48, 64)}
 end
 
 function newPersonHitboxes()
-    return {RectangleHitbox.new(4, 4, 28, 28)}
+    return {RectangleHitbox.new(8, 8, 56, 56)}
 end
 
 function newWallHitboxes(x, y)
