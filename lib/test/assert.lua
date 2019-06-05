76fb4f9eefff5fbc48c9640ad80c29654b1bdee7
tomyahu <tomyahu@gmail.com>
2019-06-05 00:13:27 -0400

    Deleted .idea files Assert file commented

--- lib/test/assert.lua
+++ lib/test/assert.lua
@@ -1,11 +1,17 @@
+-- assertEquals: object, object -> None/Error
+-- Raises an error if received is different than expected
 function assertEquals(received, expected)
     assert(received == expected)
 end
 
+-- assertFalse: bool -> None/Error
+-- Raises an error if received is true
 function assertFalse(received)
     assert(not received)
 end
 
+-- assertTrue: bool -> None/Error
+-- Raises an error if received is false
 function assertTrue(received)
     assert(received)
 end
