d2e86f0ee608bddcda0967ad9de983f8a7f10ef8
tomyahu <tomyahu@gmail.com>
2019-07-07 15:40:51 -0400

    Documented behaviours

--- Overworld/model/physics/behavior/OverworldBehavior.lua
+++ Overworld/model/physics/behavior/OverworldBehavior.lua
@@ -3,6 +3,10 @@ local NullBehavior = require "lib.physics.behaviors.NullBehavior"
 require "Global.application.application"
 --------------------------------------------------------------------------------------------------------
 
+-- class: OverworldBehavior
+-- param: group:str -> the group of the local context the behavior is associated to
+-- param: player:Player -> the player object
+-- The behaviour that defines how the player interacts with the solid objects of the overworld
 local OverworldBehavior = extend(NullBehavior, function(self, group, player)
     self.player_object = player:getSolidObject()
     self.group = group
@@ -12,17 +16,23 @@ function(group, player)
     return NullBehavior.new(group)
 end)
 
+-- AllObjectsInteract: None -> None
+-- Detects which objects collide with the player and if they collide it resets the position of the player and the
+-- object
 function OverworldBehavior.AllObjectsInteract(self)
     local local_context = application:getCurrentLocalContext()
-    
     local global_context = application:getGlobalContext()
-    
+
+    -- Sets how much time has passed for the player to define its distance moved
     self.player_object:setDt(global_context.dt)
-    
+
+    -- Sets how much time has passed for each object to define its distance moved
     for _, object in pairs(local_context[self.group]) do
         object:setDt(global_context.dt)
     end
-    
+
+    -- Checks collisions with all objects, if an object is overlapping with the player it resets its position
+    -- and sets the player to reset its position too
     local reset_player = false
     for _, object in pairs(local_context[self.group]) do
         if object:checkCollision(self.player_object) then
@@ -31,14 +41,19 @@ function OverworldBehavior.AllObjectsInteract(self)
         end
     end
 
+    -- If the player was colliding with an object its position is reset and...
     if reset_player then
         self.player_object:resetPreviousPos()
+
+        -- Resets the position every object that is colliding with the player's previous position
         for _, object in pairs(local_context[self.group]) do
             if object:checkCollision(self.player_object) then
                 object:resetPreviousPos()
             end
         end
     end
+
+    -- Sets the speed of the player to 0 in both horizontal and vertical coordinates
     self.player_object:setSpeed(0, 0)
 
 end
--- Overworld/model/physics/behavior/OverworldInteractuableObjectBehavior.lua
+++ Overworld/model/physics/behavior/OverworldInteractuableObjectBehavior.lua
@@ -3,6 +3,10 @@ local NullBehavior = require "lib.physics.behaviors.NullBehavior"
 require "Global.application.application"
 --------------------------------------------------------------------------------------------------------
 
+-- class: OverworldInteractuableObjectBehavior
+-- param: group:str -> the group of the local context the behavior is associated to
+-- param: player:Player -> the player object
+-- The behaviour that defines how the player interacts with the interactuable objects of the overworld
 local OverworldInteractuableObjectBehavior = extend(NullBehavior, function(self, group, player)
     self.player = player
     self.player_object = player:getSolidObject()
@@ -13,10 +17,11 @@ function(group)
     return NullBehavior.new(group)
 end)
 
+-- AllObjectsInteract: None -> None
+-- Checks all objects that collide with the player interactuable hitbox and makes the player interact with them
 function OverworldInteractuableObjectBehavior.AllObjectsInteract(self)
     local local_context = application:getCurrentLocalContext()
 
-    local reset_player = false
     for _, object in pairs(local_context[self.group]) do
         if object:getSolidObject():checkCollision(self.player_object) then
             object:interactWithPlayer(self.player)
