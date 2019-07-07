4c931b4fd9f887f62849382b0d5af227b8fd549c
tomyahu <tomyahu@gmail.com>
2019-07-07 19:10:34 -0400

    Documented Rooms

--- Overworld/model/rooms/GridRoomBuilder.lua
+++ Overworld/model/rooms/GridRoomBuilder.lua
@@ -7,13 +7,21 @@ local Sprite = require "Global.LOVEWrapper.sprite.Sprite"
 local SolidEntity = require "Overworld.model.entities.SolidEntity"
 require "Overworld.init.hitboxes"
 --------------------------------------------------------------------------------------------------------
-local GridRoomBuilder = class(function(self, cell_size, background_image, width, height, wall_class)
+
+-- class: GridRoomBuilder
+-- param: cell_size:int -> the size of a single cell in the room
+-- param: background_image:str -> the image to display in the background of the room
+-- param: width:int -> the width of the room in cells
+-- param: height:int -> the height of the room in cells
+-- The behaviour that defines how the player interacts with the interactuable objects of the overworld
+local GridRoomBuilder = class(function(self, cell_size, background_image, width, height)
     self.cell_size = cell_size
     self.room = Room.new(background_image, width, height)
     self.walls = {}
-    self.wall_class = wall_class
 end)
 
+-- addWall: str, int, int -> None
+-- Adds a wall in the specific cell with the image path wall_image
 function GridRoomBuilder.addWall(self, wall_image, x, y)
     local wall_frames = {}
     wall_frames[1] = {}
@@ -25,10 +33,14 @@ function GridRoomBuilder.addWall(self, wall_image, x, y)
     self.room:addObject(wall, 2)
 end
 
+-- addInvisibleWall: str, int, int -> None
+-- Adds an invisible wall in the indicated cell
 function GridRoomBuilder.addInvisibleWall(self, x, y)
     self:addWall(RESOURCES_PATH .. "/Overworld/Walls/InvisibleWall.png", x, y)
 end
 
+-- addFloor: str, int, int -> None
+-- Adds a flooring entity in de specified cell with the image path passed
 function GridRoomBuilder.addFloor(self, floor_image, x, y)
     local floor_frames = {}
     floor_frames[1] = {}
@@ -40,11 +52,15 @@ function GridRoomBuilder.addFloor(self, floor_image, x, y)
     self.room:addObject(floor, 0)
 end
 
+-- addObject: int, int, Entity -> None
+-- Adds an Entity to the room in the specified cell
 function GridRoomBuilder.addObject(self, x, y, object)
     object:setPos(x * self.cell_size, y * self.cell_size)
     self.room:addObject(object, 1)
 end
 
+-- getRoom: None -> Room
+-- Builds the room object and returns it
 function GridRoomBuilder.getRoom(self)
     return self.room
 end
--- Overworld/model/rooms/Room.lua
+++ Overworld/model/rooms/Room.lua
@@ -3,6 +3,12 @@ require "lib.algorithm.Arrays"
 local Pair = require "lib.structures.util.Pair"
 --------------------------------------------------------------------------------------------------------
 
+-- class: Room
+-- param: cell_size:int -> the size of a single cell in the room
+-- param: background_image:str -> the image to display in the background of the room
+-- param: width:int -> the width of the room in cells
+-- param: height:int -> the height of the room in cells
+-- The behaviour that defines how the player interacts with the interactuable objects of the overworld
 local Room = class(function(self, background, width, height)
     self.background_path = background
     self.width = width
@@ -21,7 +27,7 @@ function Room.addObject(self, o, layer)
 end
 
 -- getObjects: Object -> None
--- Returns the room's objects
+-- Copies object array and returns it
 function Room.getObjects(self)
     local object_array = {}
     for _, objectPair in pairs(self.objects) do
@@ -30,7 +36,7 @@ function Room.getObjects(self)
     return object_array
 end
 
--- getObjects: Object -> None
+-- getObjectsWithData: Object -> None
 -- Returns the room's objects
 function Room.getObjectsWithData(self)
     return self.objects
@@ -42,6 +48,8 @@ function Room.getBackgroundPath(self)
     return self.background_path
 end
 
+-- registerObjects: None -> None
+-- Calls the register functions of all entities in the room
 function Room.registerObjects(self)
     for _, objectPair in pairs(self.objects) do
         objectPair:getFirst():register()
