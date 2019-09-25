eec04cb0b7ea01a132b7793549b06579178f3bb5
Tomás Ahumada <tomyahu@gmail.com>
2019-09-25 00:37:06 -0300

    Documented entity factory

--- Battle/init/entity_factory.lua
+++ Battle/init/entity_factory.lua
@@ -2,14 +2,20 @@ require "lib.classes.class"
 --------------------------------------------------------------------------------------------------------
 local ENTITIES_PATH = "Battle.init.entities."
 
+-- class: EntityFactory
+-- A factory object to generate all the entities
 local EntityFactory = class(function(self)
     self.entity_dict = {}
 end)
 
+-- getEntityGenerator: str -> EntityGenerator
+-- gets the generator object that an entity of a certain type
 function EntityFactory.getEntityGenerator(self, entity_id)
     return require(ENTITIES_PATH .. entity_id)
 end
 
+-- getEntity: str -> Entity
+-- generates an entity of a certain type
 function EntityFactory.getEntity(self, entity_id)
     if pcall(function () self:getEntityGenerator(entity_id) end) then
         local entity_class = self:getEntityGenerator(entity_id)
