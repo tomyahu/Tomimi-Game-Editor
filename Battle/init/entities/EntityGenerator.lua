fc20e7140b60c75470c27f4544eb47f874894a5e
Tomás Ahumada <tomyahu@gmail.com>
2019-09-25 00:31:27 -0300

    Entity generator base class created

---
+++ Battle/init/entities/EntityGenerator.lua
@@ -0,0 +1,7 @@
+require "lib.classes.class"
+--------------------------------------------------------------------------------------------------------
+
+-- TODO: implement entity generator, the class which all the entities will inherit and will generate an entity
+local EntityGenerator = class(function(self) end)
+
+return EntityGenerator
