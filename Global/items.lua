b69af1c2575d835b76ec4f657078239bfca8651d
Tomás Ahumada <tomyahu@gmail.com>
2019-11-03 16:37:37 -0300

    updated rock description

--- Global/items.lua
+++ Global/items.lua
@@ -2,12 +2,12 @@ local items = {}
 
 local rock = {}
 rock["name"] = "Roca"
-rock["description"] = "Una roca normal."
+rock["description"] = "Una roca normal que cabe en la palma de la mano."
 rock["consumable"] = true
 rock["abilities"] = {}
 table.insert(rock["abilities"], 1)
 table.insert(rock["abilities"], 2)
 
-table.insert(items, rock)
+items[1] = rock
 
 return items
