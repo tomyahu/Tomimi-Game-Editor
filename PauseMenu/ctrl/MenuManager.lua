db409b48927e1e17be5e03e4eb5be47d963d933b
Tomás Ahumada <tomyahu@gmail.com>
2019-11-18 20:25:33 -0300

    documented

--- PauseMenu/ctrl/MenuManager.lua
+++ PauseMenu/ctrl/MenuManager.lua
@@ -1,23 +1,30 @@
 require "lib.classes.class"
 --------------------------------------------------------------------------------------------------------
--- TODO: Document this
+
+-- class: MenuManager
+-- param: side_menu:Menu -> The default menu of the pause manu
+-- param: item_menu:Menu -> The item menu of the pause manu
+-- The manager class in charge of the currently elected menu in the pause menu app
 local MenuManager = class(function(self, side_menu, item_menu)
     self.side_menu = side_menu
     self.item_menu = item_menu
     self.current_menu = side_menu
 end)
 
--- TODO: Document this
+-- callbackPressedKey: str -> None
+-- Passes the pressed key to the currently selected menu to make a transition
 function MenuManager.callbackPressedKey(self, key)
     self.current_menu:makeTransition(key)
 end
 
--- TODO: Document this
+-- setSideMenuAsCurrent: None -> None
+-- Sets the side menu as the current menu
 function MenuManager.setSideMenuAsCurrent(self)
     self.current_menu = self.side_menu
 end
 
--- TODO: Document this
+-- setItemMenuAsCurrent: None -> None
+-- Sets the item menu as the current menu
 function MenuManager.setItemMenuAsCurrent(self)
     self.current_menu = self.item_menu
 end
