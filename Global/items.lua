99cf3957900034c7acb9f9c98daf03bbc2317f84
Tomás Ahumada <tomyahu@gmail.com>
2019-11-22 17:10:28 -0300

    translated to english

--- Global/items.lua
+++ Global/items.lua
@@ -6,8 +6,8 @@ local items = {}
 
 -- Item Definition -----------------------------------------------------------------------------------------------------
 local pebble = {}
-pebble["name"] = "Roca"
-pebble["description"] = "Una roca normal que cabe en la palma de la mano."
+pebble["name"] = "Pebble"
+pebble["description"] = "A little rock that fits in the hand's palm."
 pebble["icon"] = icon_path .. "001-Pebble.png"
 pebble["consumable"] = true
 pebble["stackable"] = 20
@@ -16,16 +16,16 @@ table.insert(pebble["abilities"], 1)
 table.insert(pebble["abilities"], 2)
 
 local rope = {}
-rope["name"] = "Cuerda"
-rope["description"] = "Sirve para jugar a saltar la cuerda y atar cosas."
+rope["name"] = "Rope"
+rope["description"] = "Works for playing and jumping and tie things together."
 rope["icon"] = icon_path .. "002-Rope.png"
 rope["consumable"] = true
 rope["stackable"] = 3
 rope["abilities"] = {}
 
 local marbles = {}
-marbles["name"] = "Puñado de Canicas"
-marbles["description"] = "Son muy lindas pero hacen que la gente se caiga."
+marbles["name"] = "A Bunch of Marbles"
+marbles["description"] = "Very beautiful marbles."
 marbles["icon"] = icon_path .. "003-A-Bunch-of-Marbles.png"
 marbles["consumable"] = true
 marbles["stackable"] = 10
