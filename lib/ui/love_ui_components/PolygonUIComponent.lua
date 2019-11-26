ffb8708cd8dc78723cc5d2ff9566494739c9aba9
Tomás Ahumada <tomyahu@gmail.com>
2019-11-25 23:42:16 -0300

    added missing getter and setter

--- lib/ui/love_ui_components/PolygonUIComponent.lua
+++ lib/ui/love_ui_components/PolygonUIComponent.lua
@@ -5,9 +5,10 @@ local LoveUIComponent = require("lib.ui.love_ui_components.LoveUIComponent")
 -- class: PolygonUIComponent
 -- param: mode:("fill" or "line") -> defines the way to define the polygon, "fill" fills the area and
 --                                      "line" just draws the outline
--- param: vertices:list(int) -> an array of intercalated x and y coordinates that represent the polygon's vertices
--- param: x:int -> the offset x coordinate to move all vertices
--- param: y:int -> the offset y coordinate to move all vertices
+-- param: vertices:list(num) -> an array of intercalated x and y coordinates that represent the polygon's vertices
+-- param: x:num -> the offset x coordinate to move all vertices
+-- param: y:num -> the offset y coordinate to move all vertices
+-- A wrapper of love's polygon drawable
 local PolygonUIComponent = extend(LoveUIComponent, function(self, mode, vertices, x, y)
     self.mode = mode
     self.x = x
@@ -28,6 +29,10 @@ function PolygonUIComponent.draw(self)
 end
 
 -- getters
+function PolygonUIComponent.getMode(self)
+    return self.mode
+end
+
 function PolygonUIComponent.getX(self)
     return self.x
 end
@@ -45,6 +50,10 @@ function PolygonUIComponent.getVertices(self)
 end
 
 -- setters
+function PolygonUIComponent.setMode(self, new_mode)
+    self.mode = new_mode
+end
+
 function PolygonUIComponent.setX(self, new_x)
     for i = 1,(# self.raw_vertices),2 do
         self.vertices[i] = self.raw_vertices[i] - self.x + new_x
