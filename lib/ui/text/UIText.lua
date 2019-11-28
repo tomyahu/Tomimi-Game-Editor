5647c9da009a27bfa7d64229c3ce64afd42086e0
Tomás Ahumada <tomyahu@gmail.com>
2019-11-28 20:03:00 -0300

    added msg to UIText

--- lib/ui/text/UIText.lua
+++ lib/ui/text/UIText.lua
@@ -2,16 +2,22 @@ require "lib.classes.class"
 --------------------------------------------------------------------------------------------------------
 
 -- class: UIText
+-- param: msg:str -> the string to draw
 -- param: x:num -> the x coordinate of the top left on where to draw the text
 -- param: y:num -> the y coordinate of the top left on where to draw the text
 -- param: font:love.Font -> the font of the text
-local UIText = class(function(self, x, y, font)
+local UIText = class(function(self, msg, x, y, font)
+    self.msg = msg
     self.x = x
     self.y = y
     self.font = font
 end)
 
 -- getters
+function UIText.getMsg(self)
+    return self.msg
+end
+
 function UIText.getX(self)
     return self.x
 end
@@ -25,6 +31,10 @@ function UIText.getFont(self)
 end
 
 -- setters
+function UIText.setMsg(self, new_msg)
+    self.msg = new_msg
+end
+
 function UIText.setX(self, new_x)
     self.x = new_x
 end
