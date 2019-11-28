64f32fe2bb3bc0683fb171f943c905e988c273a6
Tomás Ahumada <tomyahu@gmail.com>
2019-11-28 19:57:57 -0300

    added UIText

---
+++ lib/ui/text/UIText.lua
@@ -0,0 +1,40 @@
+require "lib.classes.class"
+--------------------------------------------------------------------------------------------------------
+
+-- class: UIText
+-- param: x:num -> the x coordinate of the top left on where to draw the text
+-- param: y:num -> the y coordinate of the top left on where to draw the text
+-- param: font:love.Font -> the font of the text
+local UIText = class(function(self, x, y, font)
+    self.x = x
+    self.y = y
+    self.font = font
+end)
+
+-- getters
+function UIText.getX(self)
+    return self.x
+end
+
+function UIText.getY(self)
+    return self.y
+end
+
+function UIText.getFont(self)
+    return self.font
+end
+
+-- setters
+function UIText.setX(self, new_x)
+    self.x = new_x
+end
+
+function UIText.setY(self, new_y)
+    self.y = new_y
+end
+
+function UIText.getFont(self, new_font)
+    self.font = new_font
+end
+
+return UIText
