c380ff1ae2caab5f4f2826b75a489e1b25fd8c80
Tomás Ahumada <tomyahu@gmail.com>
2019-12-03 13:53:21 -0300

    adapted TextUIComponent to extend UIComponent

--- lib/ui/love_ui_components/TextUIComponent.lua
+++ lib/ui/love_ui_components/TextUIComponent.lua
@@ -1,5 +1,6 @@
 require "lib.classes.class"
 require "Global.LOVEWrapper.LOVEWrapper"
+local LoveUIComponent = require("lib.ui.love_ui_components.LoveUIComponent")
 --------------------------------------------------------------------------------------------------------
 
 -- class: TextUIComponent
@@ -7,33 +8,27 @@ require "Global.LOVEWrapper.LOVEWrapper"
 -- param: x:num -> the x coordinate of the top left on where to draw the text
 -- param: y:num -> the y coordinate of the top left on where to draw the text
 -- param: font:love.Font -> the font of the text
--- TODO: Adapt this to extend UIComponent
-local TextUIComponent = class(function(self, msg, x, y, font)
+-- param: color:{num, num, num, num} -> the color of the text
+-- A component to show text on the screen
+local TextUIComponent = extend(LoveUIComponent, function(self, msg, x, y, font, color)
     self.msg = msg
-    self.x = x
-    self.y = y
     self.font = font
     self.text = love.graphics.newText( font, msg )
+end,
+function(msg, x, y, font, color)
+    return LoveUIComponent.new(nil, x, y, color)
 end)
 
 -- draw: None -> None
 -- draws the text in the screen according to the object's parameters
 function TextUIComponent.draw(self)
-    love.graphics.setColor(255, 255, 255)
+    love.graphics.setColor( self.color )
     love.graphics.draw(self.text, getRelativePosX(self.x), getRelativePosY(self.y), 0, getScale(), getScale())
 end
 
 -- getters
 function TextUIComponent.getMsg(self)
     return self.msg
-end
-
-function TextUIComponent.getX(self)
-    return self.x
-end
-
-function TextUIComponent.getY(self)
-    return self.y
 end
 
 function TextUIComponent.getFont(self)
@@ -51,14 +46,6 @@ end
 -- setters
 function TextUIComponent.setMsg(self, new_msg)
     self.msg = new_msg
-end
-
-function TextUIComponent.setX(self, new_x)
-    self.x = new_x
-end
-
-function TextUIComponent.setY(self, new_y)
-    self.y = new_y
 end
 
 function TextUIComponent.getFont(self, new_font)
