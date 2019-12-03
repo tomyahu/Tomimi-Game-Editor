42640e8169e6f43614322bb2521856da8153d9ce
Tomás Ahumada <tomyahu@gmail.com>
2019-12-03 13:29:48 -0300

    changed UIText to TextUIComponent added UICropWrapper adapted UILimitedText

--- lib/ui/love_ui_components/TextUIComponent.lua
+++ lib/ui/love_ui_components/TextUIComponent.lua
@@ -2,12 +2,13 @@ require "lib.classes.class"
 require "Global.LOVEWrapper.LOVEWrapper"
 --------------------------------------------------------------------------------------------------------
 
--- class: UITextComponent
+-- class: TextUIComponent
 -- param: msg:str -> the string to draw
 -- param: x:num -> the x coordinate of the top left on where to draw the text
 -- param: y:num -> the y coordinate of the top left on where to draw the text
 -- param: font:love.Font -> the font of the text
-local UITextComponent = class(function(self, msg, x, y, font)
+-- TODO: Adapt this to extend UIComponent
+local TextUIComponent = class(function(self, msg, x, y, font)
     self.msg = msg
     self.x = x
     self.y = y
@@ -17,43 +18,51 @@ end)
 
 -- draw: None -> None
 -- draws the text in the screen according to the object's parameters
-function UITextComponent.draw(self)
+function TextUIComponent.draw(self)
     love.graphics.setColor(255, 255, 255)
     love.graphics.draw(self.text, getRelativePosX(self.x), getRelativePosY(self.y), 0, getScale(), getScale())
 end
 
 -- getters
-function UITextComponent.getMsg(self)
+function TextUIComponent.getMsg(self)
     return self.msg
 end
 
-function UITextComponent.getX(self)
+function TextUIComponent.getX(self)
     return self.x
 end
 
-function UITextComponent.getY(self)
+function TextUIComponent.getY(self)
     return self.y
 end
 
-function UITextComponent.getFont(self)
+function TextUIComponent.getFont(self)
     return self.font
 end
 
+function TextUIComponent.getWidth(self)
+    return self.text:getWidth()
+end
+
+function TextUIComponent.getHeight(self)
+    return self.text:getHeight()
+end
+
 -- setters
-function UITextComponent.setMsg(self, new_msg)
+function TextUIComponent.setMsg(self, new_msg)
     self.msg = new_msg
 end
 
-function UITextComponent.setX(self, new_x)
+function TextUIComponent.setX(self, new_x)
     self.x = new_x
 end
 
-function UITextComponent.setY(self, new_y)
+function TextUIComponent.setY(self, new_y)
     self.y = new_y
 end
 
-function UITextComponent.getFont(self, new_font)
+function TextUIComponent.getFont(self, new_font)
     self.font = new_font
 end
 
-return UITextComponent
+return TextUIComponent
--- lib/ui/text/UILimitedText.lua
+++ lib/ui/text/UILimitedText.lua
@@ -1,6 +1,7 @@
 require "lib.classes.class"
 require "Global.LOVEWrapper.LOVEWrapper"
-local UIText = require("lib.ui.love_ui_components.UITextComponent")
+local UICropWrapper = require("lib.ui.ui_wrapper.UICropWrapper")
+local TextUIComponent = require("lib.ui.love_ui_components.TextUIComponent")
 --------------------------------------------------------------------------------------------------------
 
 -- class: UILimitedText
@@ -10,50 +11,12 @@ local UIText = require("lib.ui.love_ui_components.UITextComponent")
 -- param: font:love.Font -> the font of the limited text
 -- param: width:num -> the width of the limiter rectangle for the text
 -- param: height:num -> the height of the limiter rectangle for the text
-local UILimitedText = extend(UIText, function(self, msg, x, y, font, width, height)
-    self.width = width
-    self.height = height
-    self.canvas = love.graphics.newCanvas( width, height )
-    self.canvas:renderTo(function()
-        love.graphics.setColor(255, 255, 255)
-        love.graphics.draw(self.text)
-    end)
+-- Text UI component that crops the text in a rectangle of dimensions width and height
+local UILimitedText = extend(UICropWrapper, function(self, msg, x, y, font, width, height)
+end,
+function(msg, x, y, font, width, height)
+    local text = UITextComponent.new(msg, x, y, font)
+    return UICropWrapper.new(x,y,text,width,height)
 end)
-
--- draw: None -> None
--- draws the limited text in the screen according to the object's parameters
-function UILimitedText.draw(self)
-    love.graphics.draw(self.canvas, getRelativePosX(self.x), getRelativePosY(self.y), getScale(), getScale())
-end
-
--- getters
-function UILimitedText.getWidth(self)
-    return self.width
-end
-
-function UILimitedText.getHeight(self)
-    return self.height
-end
-
--- setters
-function UILimitedText.setWidth(self, new_width)
-    self.width = new_width
-    self:redefineCanvas()
-end
-
-function UILimitedText.setHeight(self, new_height)
-    self.height = new_height
-    self:redefineCanvas()
-end
-
--- redefineCanvas: None -> None
--- redefines the contents of the canvas
-function UILimitedText.redefineCanvas(self)
-    self.canvas = love.graphics.newCanvas( self.width, self.height )
-    self.canvas:renderTo(function()
-        love.graphics.setColor(255, 255, 255)
-        love.graphics.draw(self.text)
-    end)
-end
 
 return UILimitedText
