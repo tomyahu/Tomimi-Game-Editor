0e1b04d7838a680b359a4cb04819052c1342afba
Tomás Ahumada <tomyahu@gmail.com>
2020-01-07 20:15:00 -0300

    created action factory and actions files

---
+++ Battle/init/actions/ActionFactory.lua
@@ -0,0 +1,3 @@
+
+
+-- TODO: Implement Action Factory with flyweight pattern
---
+++ Battle/init/actions/actions.lua
@@ -0,0 +1,8 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: tomya
+-- Date: 07-01-2020
+-- Time: 20:10
+-- To change this template use File | Settings | File Templates.
+--
+
