ff5cf9ecaef8e366234cc79b701736041473bf58
Tomás Ahumada <tomyahu@gmail.com>
2020-01-08 18:16:36 -0300

    ambients will no longer have effects on the battle

--- Battle/model/ambients/NullAmbient.lua
+++ Battle/model/ambients/NullAmbient.lua
@@ -8,10 +8,6 @@ local NullAmbient = class(function(self, path)
     self.sprite_path = path
 end)
 
--- affectPartyAndEnemies: None -> None
--- Applies the effects of the ambient to the player and enemy parties
-function NullAmbient.affectPartyAndEnemies(self, player_party, enemy_party) end
-
 --getter
 function NullAmbient.getSpritePath(self)
     return self.sprite_path
