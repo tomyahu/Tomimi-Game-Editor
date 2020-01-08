41092ceff4953a477e809d05427befca0f4cb682
Tomás Ahumada <tomyahu@gmail.com>
2020-01-08 18:34:36 -0300

    added null action as first action

--- Battle/init/actions/actions.lua
+++ Battle/init/actions/actions.lua
@@ -4,9 +4,20 @@ local ActionBuilder = require("Battle.model.actions.ActionBuilder")
 local actions = {}
 local action_build = ActionBuilder.new()
 
--- 1. Perfect Escape
+-- 1. Do Nothing -------------------------------------------------------------------------------------------------------
 action_build:reset()
 action_build:setId(1)
+action_build:setName("Do Nothing")
+action_build:setDescription("Wastes a turn doing nothing.")
+action_build:setStartPiece("N")
+action_build:setEndPiece("N")
+action_build:setActionFunction( function(source_entity, target_entity)
+end)
+table.insert(actions, action_build:getAction())
+
+-- 2. Perfect Escape ---------------------------------------------------------------------------------------------------
+action_build:reset()
+action_build:setId(2)
 action_build:setName("Perfect Escape")
 action_build:setDescription("Escapes from combat with a 100% chance.")
 action_build:setStartPiece("N")
@@ -14,5 +25,6 @@ action_build:setEndPiece("N")
 action_build:setActionFunction( function(source_entity, target_entity)
     -- TODO: get ctrl and call escape function
 end)
+table.insert(actions, action_build:getAction())
 
 return actions
