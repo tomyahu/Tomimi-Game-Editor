d222e3655dfbfb1aa4d99f7cba8023834fbb2e23
Tomás Ahumada <tomyahu@gmail.com>
2020-01-16 17:42:37 -0300

    finished random action turn AI

--- Battle/model/turns/RandomActionTurn.lua
+++ Battle/model/turns/RandomActionTurn.lua
@@ -2,6 +2,7 @@ require "lib.classes.class"
 require "Global.application.application"
 require "Battle.consts"
 local Turn = require("Battle.model.turns.Turn")
+local ActionSequenceCreator = require("Battle.model.action_sequence.ActionSquenceCreator")
 --------------------------------------------------------------------------------------------------------
 
 -- class: RandomActionTurn
@@ -21,7 +22,18 @@ function RandomActionTurn.start(self)
     local possible_actions = self.entity:getActions()
 
     -- Choose one action at random
-    local actions = { possible_actions[math.random(1,(# possible_actions))] }
+    local action_sequence_creator = ActionSequenceCreator.new(possible_actions)
+    local start_actions = action_sequence_creator:getStartActions()
+    action_sequence_creator:addAction(start_actions[math.random(1, (# start_actions))])
+    while action_sequence_creator:getLastAction():isEndAction() do
+        local last_action_type = action_sequence_creator:getLastAction():getEndPiece()
+        local next_action = action_sequence_creator:getActionsWithStartType(last_action_type)
+        local compatible_actions = action_sequence_creator:addAction(next_action)
+        action_sequence_creator:addAction(compatible_actions[math.random(1, (# compatible_actions))])
+    end
+
+    local actions = action_sequence_creator:getActionSequence()
+
 
     -- Choose random targets depending on the first action
     local target_getter = ctrl:getTargetGetter()
