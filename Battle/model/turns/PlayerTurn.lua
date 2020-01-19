4ecc45d9612b3245766fcb5edb5d665bbe9a467a
Tomás Ahumada <tomyahu@gmail.com>
2020-01-19 01:49:42 -0300

    created starting attack menu

--- Battle/model/turns/PlayerTurn.lua
+++ Battle/model/turns/PlayerTurn.lua
@@ -8,125 +8,126 @@ local MenuState = require "Menu.model.menuStates.MenuState"
 local SingleActionMenuState = require "Menu.model.menuStates.SingleActionMenuState"
 local ContentMenuState = require "Menu.model.menuStates.ContentMenuState"
 local ActionSequenceCreator = require("Battle.model.action_sequence.ActionSequenceCreator")
+
+local ActionFactory = require("Battle.init.actions.ActionFactory")
 --------------------------------------------------------------------------------------------------------
+
+local flee_action = ActionFactory.getActionWithID(2)
 
 -- class: PlayerTurn
 -- param: entity:Entity -> The entity the turn is from
 -- A turn of an entity in a battle where the player chooses the abilities to use
 local PlayerTurn = extend(Turn, function(self, entity)
     self.entity = entity
-    self.menues = self:makeMenu()
+    self.menues = {}
+    self.selected_target = entity
 end)
 
 -- start: None -> None
 -- Starts a new edition of the turn of this entity
 function PlayerTurn.start(self)
     local ctrl = application:getCurrentCtrl()
-    self.menues = self:makeMenu()
+    self:makeMenues()
+    print("made menues")
 
     local menu_manager = ctrl:getMenuManager()
 
     -- Set menu to the manager
-    menu_manager:setCurrentMenu(self.menues["start_action_menu"])
+    menu_manager:setCurrentMenu(self.menues.action_menu)
 end
 
--- makeMenu: None -> Menu
+-- makeMenues: None -> Menu
 -- TODO: Document this
-function PlayerTurn.makeMenu(self)
-    local aux = {}
+function PlayerTurn.makeMenues(self)
+    -- Create Basic Action Type Menu
+    self:makeBasicActionMenu()
+
+    -- Create Attack Action Menu
+    self.makeAttackStartActionMenu()
+
+    -- TODO: Create Support Action Menu
+end
+
+
+-- makeBasicActionMenu: None -> None
+-- Creates the first menu to show up when its the player turn
+function PlayerTurn.makeBasicActionMenu(self)
     local ctrl = application:getCurrentCtrl()
     local menu_manager = ctrl:getMenuManager()
     local turn_manager = ctrl:getTurnManager()
 
+    local m_build = DefaultMenuBuilder.new()
 
-    -- Create Start Action Menu
-    local start_action_menu = self:getStartActionMenuWithActions(aux)
-    aux["start_action_menu"] = start_action_menu
+    -- TODO: Check if player has starting attack moves, if not show this state as ??? with no action
+    local attack_state = SingleActionMenuState.new("Attack", ACTION_BUTTON_1, function (_)
+        error("Attack selected.")
+        menu_manager:setCurrentMenu(self.menues.start_attack_action_menu)
+    end)
 
-    return aux
+    -- TODO: Check if player has starting support moves, if not show this state as ??? with no action
+    local support_state = SingleActionMenuState.new("Support", ACTION_BUTTON_1, function (_)
+        error("Support selected.")
+        menu_manager:setCurrentMenu(self.menues.start_support_action_menu)
+    end)
+
+    local flee_state = SingleActionMenuState.new("Run Away", ACTION_BUTTON_1, function (_)
+        print("Run Away selected.")
+        turn_manager:turnEnded({flee_action}, {self.entity})
+    end)
+
+    m_build:addState(attack_state)
+    m_build:addState(support_state)
+    m_build:addState(flee_state)
+
+    self.menues.action_menu = m_build:getMenu()
 end
 
--- getStartActionMenuWithActions: dict -> Menu
--- The menu when an action is started.
--- TODO: If non ending actions was selected move to combo menu, else move to target menu
--- TODO: Reimplement this changing menues to self.data
-function PlayerTurn.getStartActionMenuWithActions(self, menues)
+-- makeAttackStartActionMenu: None -> None
+-- Creates the menu that shows up when the attack option is selected in the first menu
+function PlayerTurn.makeAttackStartActionMenu(self)
     local ctrl = application:getCurrentCtrl()
     local menu_manager = ctrl:getMenuManager()
 
+    local menu_pointer_table = {}
+
     local m_build = DefaultMenuBuilder.new()
 
-    local possible_actions = self.entity:getActions()
-    menues["action_sequence_creator"] = ActionSequenceCreator.new(possible_actions)
+    -- Get starting attack actions
+    local entity_actions = self.entity:getActions()
+    local action_sequence_creator = ActionSequenceCreator.new(entity_actions)
 
-    local actions = menues["action_sequence_creator"]:getStartActions()
-    local target_getter = ctrl:getTargetGetter()
+    local start_actions = action_sequence_creator:getStartAttackActions()
 
-    for _, action in pairs(actions) do
-        local action_state = SingleActionMenuState.new(action:getName(), ACTION_BUTTON_1, function (_)
-            print(action:getName() .. " action selected as start action.")
-            local target_type = action:getTarget()
-            menues["action_sequence_creator"]:addAction(action)
+    -- Create back funtion
+    local back_funtion = function()
+        menu_manager:setCurrentMenu(self.menues.action_menu)
+    end
 
-            local targets = target_getter:getTargets(self.entity, action:getTarget())
+    -- TODO: Create menu for all starting actions
+    for _, action in pairs(start_actions) do
+        local action_state = ContentMenuState.new(action:getName(), action)
 
-            menues["target_menu"] = self:getSelectorMenuWithTargets(menues, targets)
+        action:addTransitionAction(ACTION_BUTTON_1, function(_)
+            action_sequence_creator:addAction(action)
 
-            menu_manager:setCurrentMenu(menues["target_menu"])
+            -- TODO: If action is an ending action go to the target selection menu
+            if action:isEndAction() then
+                error("Target Selection Menu is not implemented yet")
+                local target_menu = self:makeTargetMenu(menu_pointer_table, action_sequence_creator:getActionSequence())
+                menu_manager:setCurrentMenu(target_menu)
+            end
+
+            error("Combo Menu is not implemented yet")
+            -- TODO: Otherwise create an attack combo menu with this menu as the previous menu and go to it
+            local combo_menu = self:makeAttackComboMenu(menu_pointer_table, action_sequence_creator)
+            menu_manager:setCurrentMenu(combo_menu)
+
         end)
         m_build:addState(action_state)
     end
 
-    return m_build:getMenu()
-end
-
--- getSelectorMenuWithTargets: dict, list(list(Entity)) -> Menu
--- gets a menu to select a target for an action sequence
--- TODO: Reimplement this changing menues to self.data
--- TODO: move this to the end, if special action was selected do what the
-function PlayerTurn.getSelectorMenuWithTargets(self, menues, targets)
-    local ctrl = application:getCurrentCtrl()
-    local menu_manager = ctrl:getMenuManager()
-    local turn_manager = ctrl:getTurnManager()
-
-    local target_build = DefaultMenuBuilder.new()
-
-    local back_function = function(_)
-        menu_manager:setCurrentMenu(menues["start_action_menu"])
-        print("back to first menu")
-    end
-
-    local target_states = {}
-    for i, target in pairs(targets) do
-        local new_target_state = ContentMenuState.new(i, target)
-
-        new_target_state:addTransitionAction(ACTION_BUTTON_1, function(_)
-            -- TODO: if action sequence creator has end action... end turn
-            if menues["action_sequence_creator"]:getLastAction():isEndAction() then
-                -- end turn
-                turn_manager:turnEnded(menues["action_sequence_creator"]:getActionSequence(), {target})
-            else
-                --else go to combo action menu
-                menues["targets"] = target
-
-                local possible_actions = menues["action_sequence_creator"]:getActionsCompatibleWithLastAction()
-                menues["combo_action_menu"] = self:getComboMenuWithActions(possible_actions)
-
-                menu_manager:setCurrentMenu(menues["combo_action_menu"])
-            end
-        end)
-
-        new_target_state:addTransitionAction(ACTION_BUTTON_2, back_function)
-
-        target_build:addState(new_target_state)
-    end
-
-    return target_build:getMenu()
-end
-
--- getComboMenuWithActions: list(Action) -> Menu
--- Gets a next combo menu based on the given actions
-function PlayerTurn.getComboMenuWithActions(self, actions)
+    menu_pointer_table.menu = m_build:getMenu()
+    self.menues.start_attack_action_menu = menu_pointer_table.menu
 end
 
 return PlayerTurn
