560ea54da3ecbaa89647bede0c95bec5b8721afc
Tomás Ahumada <tomyahu@gmail.com>
2020-01-20 16:26:35 -0300

    added menu resources to debug init

--- Battle/init/_debug_init.lua
+++ Battle/init/_debug_init.lua
@@ -1,9 +1,14 @@
+require "Global.consts"
+require "Global.fonts"
+
 require "lib.classes.class"
 local BattleCtrl = require "Battle.ctrl.BattleCtrl"
 local BattleView = require "Battle.view.BattleView"
 --------------------------------------------------------------------------------------------------------
+-- Set Font
+local font = DIALOG_FONT
 
-local view = BattleView.new()
+local view = BattleView.new(RESOURCES_PATH .. "/PauseMenu/MenuBorders.png", font)
 local ctrl = BattleCtrl.new(view)
 
 return {["ctrl"] = ctrl, ["view"] = view}
