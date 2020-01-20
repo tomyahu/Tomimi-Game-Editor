6e149a9233d29b9c085b2c5cb80aa91113d5404c
Tomás Ahumada <tomyahu@gmail.com>
2020-01-20 18:04:19 -0300

    deleted is_over parameter

--- Battle/model/turns/Turn.lua
+++ Battle/model/turns/Turn.lua
@@ -6,7 +6,6 @@ require "lib.classes.class"
 -- A turn of an entity in a battle
 local Turn = class(function(self, entity)
     self.entity = entity
-    self.is_over = false
 end)
 
 -- start: None -> None
@@ -17,18 +16,9 @@ function Turn.toString(self)
     return self.entity:getName() .. "'s Turn."
 end
 
--- setters
-function Turn.setOverStatus(self, over_status)
-    self.is_over = over_status
-end
-
 -- getters
 function Turn.getEntity(self)
     return self.entity
-end
-
-function Turn.isOver(self)
-    return self.is_over
 end
 
 return Turn
