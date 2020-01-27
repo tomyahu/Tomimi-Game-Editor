3615191d953f41381d9a8e1ed1f9b4e7b87df209
Tomás Ahumada <tomyahu@gmail.com>
2020-01-27 16:46:48 -0300

    added acctor name to animation sequence builder

--- Battle/view/animations/AnimationSequenceBuilder.lua
+++ Battle/view/animations/AnimationSequenceBuilder.lua
@@ -5,12 +5,13 @@ local AnimationSequence = require("Battle.view.animations.AnimationSequence")
 -- class: AnimationSequenceBuilder
 -- A builder class for a sequence of animations to be played
 local AnimationSequenceBuilder = class(function(self)
+    self.actors = {}
     self.animations = {}
 end)
 
--- addAnimation: Animation, num(0,), num(0,) -> AnimationSequenceBuilder
--- Adds an animation with specific start and end time
-function AnimationSequenceBuilder.addAnimation(self, animation, start_time, end_time)
+-- addAnimation: Animation, num(0,), num(0,), str -> AnimationSequenceBuilder
+-- Adds an animation with specific start, end time and actor name
+function AnimationSequenceBuilder.addAnimation(self, animation, start_time, end_time, actor_name)
     if animation == nil then
         error("Expected a table for animation, got null value.")
     end
@@ -23,9 +24,14 @@ function AnimationSequenceBuilder.addAnimation(self, animation, start_time, end_
         error("Expected a number for end_time, got null value.")
     end
 
+    if actor_name == nil then
+        error("Expected a string for actor_name, got null value.")
+    end
+
     local aux = {}
     table.insert(aux, start_time)
     table.insert(aux, end_time)
+    table.insert(aux, actor_name)
 
     self.animations[animation] = aux
     return self
