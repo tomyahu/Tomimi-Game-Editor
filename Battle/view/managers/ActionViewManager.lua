8b720f17d83c01ace15158eb8810db952ff18991
Tomás Ahumada <tomyahu@gmail.com>
2020-01-27 18:13:48 -0300

    postponed ActionViewManager implementation

---
+++ Battle/view/managers/ActionViewManager.lua
@@ -0,0 +1,10 @@
+require "lib.classes.class"
+--------------------------------------------------------------------------------------------------------
+
+-- class: ActionViewManager
+-- A manager class to handle animations for actions
+local ActionViewManager = class(function(self)
+end)
+
+return ActionViewManager
+
