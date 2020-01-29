3d747f3aa8518632b0d7eef78b8ce7b95ceb6464
Tomás Ahumada <tomyahu@gmail.com>
2020-01-29 17:30:43 -0300

    added default and null animations

--- Battle/init/action_animations/action_animations.lua
+++ Battle/init/action_animations/action_animations.lua
@@ -1,17 +1,27 @@
+require "Global.consts"
+
 local actions = require("Battle.init.actions.actions")
-local AnimationSequenceBuilder = require("Battle.view.animations.AnimationSequenceBuilder")
-local ActionAnimations = require("Battle.view.animations.AnimationSequenceBuilder")
+local AnimationSequenceBuilder = require("Battle.view.animations.animation_sequence.AnimationSequenceBuilder")
+local ActionAnimations = require("Battle.view.animations.action_animations.ActionAnimations")
+
+local LinearMoveAnimation = require("Battle.view.animations.animations.LinearMoveAnimation")
+local LinearReturnMoveAnimation = require("Battle.view.animations.animations.LinearReturnMoveAnimation")
 --------------------------------------------------------------------------------------------------------
 
 local animation_dict = {}
 
--- TODO: Create default animation to create for all actions
-local default_animation = nil
+-- An animation where nothing is done
+local null_animation_builder = AnimationSequenceBuilder.new()
+
+-- Create default animation to create for all actions
+local default_animation_builder = AnimationSequenceBuilder.new()
+default_animation_builder:addAnimation(LinearMoveAnimation.new(GAME_WIDTH/2, 400/600*GAME_HEIGHT, 0.5), 0, 0.5)
+default_animation_builder:addAnimation(LinearReturnMoveAnimation.new(0.5), 1.5, 2)
 
 
 -- Fill all actions with default animation
 for _, action in pairs(actions) do
-    animation_dict[action] = default_animation
+    animation_dict[action] = ActionAnimations.new(default_animation_builder, null_animation_builder)
 end
 
 return animation_dict
