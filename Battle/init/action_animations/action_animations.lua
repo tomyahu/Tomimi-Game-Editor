85f708be3c1f4303f03f9d9a7c0ed0d9727ccdef
Tomás Ahumada <tomyahu@gmail.com>
2020-02-05 19:20:08 -0300

    repositioned abilities to work with the new system

--- Battle/init/action_animations/action_animations.lua
+++ Battle/init/action_animations/action_animations.lua
@@ -15,16 +15,11 @@ local animation_dict = {}
 -- An animation where nothing is done
 local null_animation_builder = AnimationSequenceBuilder.new()
 
--- Create default animation to create for all actions
-local default_animation_builder = AnimationSequenceBuilder.new()
-default_animation_builder:addAnimation(LinearMoveAnimation.new(GAME_WIDTH/2, 400/600*GAME_HEIGHT, 0.5), 0, 0.5)
-default_animation_builder:addAnimation(LinearReturnMoveAnimation.new(0.5), 1.5, 2)
-default_animation_builder:addAnimation(ApplyActionAnimation.new(), 1.5, 2)
-
 -- Fill all actions with default animation
 for _, action in pairs(actions) do
+    -- Create default animation to create for all actions
     local default_animation_builder = AnimationSequenceBuilder.new()
-    default_animation_builder:addAnimation(LinearMoveAnimation.new(GAME_WIDTH/2, 400/600*GAME_HEIGHT, 0.5), 0, 0.5)
+    default_animation_builder:addAnimation(LinearMoveAnimation.new(GAME_WIDTH/2, 450/600*GAME_HEIGHT, 0.5), 0, 0.5)
     default_animation_builder:addAnimation(LinearReturnMoveAnimation.new(0.5), 1.5, 2)
     default_animation_builder:addAnimation(ApplyActionAnimation.new(action, {}), 1.5, 1.5)
     animation_dict[action] = ActionAnimations.new(default_animation_builder, null_animation_builder)
@@ -35,7 +30,7 @@ end
 ------------------------------------------------------------------------------------------------------------------------
 local run_away_action = actions[2]
 local run_away_animation_builder = AnimationSequenceBuilder.new()
-run_away_animation_builder:addAnimation(LinearMoveAnimation.new(GAME_WIDTH/2, 350/600*GAME_HEIGHT, 0.5), 0, 0.5)
+run_away_animation_builder:addAnimation(LinearMoveAnimation.new(GAME_WIDTH/2, 450/600*GAME_HEIGHT, 0.5), 0, 0.5)
 run_away_animation_builder:addAnimation(
     ApplyActionAnimation.new(run_away_action),
     0.5,
@@ -51,7 +46,7 @@ animation_dict[run_away_action] = ActionAnimations.new(run_away_animation_builde
 ------------------------------------------------------------------------------------------------------------------------
 local feel_proud_action = actions[3]
 local feel_proud_animation_builder = AnimationSequenceBuilder.new()
-feel_proud_animation_builder:addAnimation(LinearMoveAnimation.new(GAME_WIDTH/2, 350/600*GAME_HEIGHT, 0.5), 0, 0.5)
+feel_proud_animation_builder:addAnimation(LinearMoveAnimation.new(GAME_WIDTH/2, 450/600*GAME_HEIGHT, 0.5), 0, 0.5)
 feel_proud_animation_builder:addAnimation(
     MessageDisplayAnimation.new("The Naranjarina is feeling proud of themself. Good for you Naranjarina.", 2),
     0.5,
@@ -66,7 +61,7 @@ animation_dict[feel_proud_action] = ActionAnimations.new(feel_proud_animation_bu
 ------------------------------------------------------------------------------------------------------------------------
 local true_escape_action = actions[7]
 local true_escape_animation_builder = AnimationSequenceBuilder.new()
-true_escape_animation_builder:addAnimation(LinearMoveAnimation.new(GAME_WIDTH/2, 350/600*GAME_HEIGHT, 0.5), 0, 0.5)
+true_escape_animation_builder:addAnimation(LinearMoveAnimation.new(GAME_WIDTH/2, 450/600*GAME_HEIGHT, 0.5), 0, 0.5)
 true_escape_animation_builder:addAnimation(
 ApplyActionAnimation.new(true_escape_action),
 0.5,
