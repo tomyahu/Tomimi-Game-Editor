1403fce9c21e007a9e69b422583bd8e81e6bf43c
Tomás Ahumada <tomyahu@gmail.com>
2020-02-05 18:19:02 -0300

    documented party menu view

--- PauseMenu/view/menus/PartyMenuView.lua
+++ PauseMenu/view/menus/PartyMenuView.lua
@@ -1,17 +1,20 @@
 require "lib.classes.class"
 --------------------------------------------------------------------------------------------------------
--- TODO: Document this
--- TODO: Generalize this for more tools
+
+-- class: PartyMenuView
+-- param: background_border:UITeselatedFrame -> a frame that serves as the background of the party menu view
+-- param: character_cards:list(CharacterInfoCard) -> a list of the character information cards to display
+-- The party menu view class to display the state of the party characters
 local PartyMenuView = class(function(self, background_border, character_cards)
     self.background_border = background_border
     self.character_cards = character_cards
-    -- TODO: Create Menu in ctrl that just have a back state
 end)
 
--- TODO: Document this
+-- draw: None -> None
+-- draws the background and the character cards of the party
 function PartyMenuView.draw(self)
     self.background_border:draw()
-    for index, card in pairs(self.character_cards) do
+    for _, card in pairs(self.character_cards) do
         card:draw()
     end
 end
