b870cdf34058aa6cd6808b49ab73d6f1fc23afd5
Tomás Ahumada <tomyahu@gmail.com>
2020-02-07 17:05:10 -0300

    polished character portraits

--- Battle/view/party/party_stats/PartyStats.lua
+++ Battle/view/party/party_stats/PartyStats.lua
@@ -12,24 +12,25 @@ local PartyStats = class(function(self, entities)
     self.guard_color = {0.5,0.5,0.5,1}
 
     local portrait_background_path = RESOURCES_PATH .. "/Battle/Miscelaneous/PortraitContainers/PortraitStatContainer.png"
-    self.entity_portrait_background = SpriteFactory.getRegularRectSprite(portrait_background_path, 128, 96, 1)
+    self.entity_portrait_background = SpriteFactory.getRegularRectSprite(portrait_background_path, 200, 100, 1)
     self.entities = entities
     self.entities_portraits = {}
     for _, entity in pairs(self.entities) do
         local portrait_path = entity:getSpriteFolderPath() .. "portrait.png"
-        table.insert(self.entities_portraits, SpriteFactory.getRegularRectSprite(portrait_path, 128, 128, 1))
+        table.insert(self.entities_portraits, SpriteFactory.getRegularRectSprite(portrait_path, 80, 80, 1))
     end
 
-    self.scale_factor = 1.25
     self.initial_offset_y = 150
-    self.offset_stat_bars = 31*self.scale_factor
 
-    self.space_x = 102*self.scale_factor
-    self.space_x_portraits = 10*self.scale_factor
-    self.space_y = 15*self.scale_factor
-    self.space_between_y = 30*self.scale_factor
+    self.offset_x_stat_bars = 132
+    self.offset_y_stat_bars = 37
+    self.offset_x_portraits = 10
+    self.offset_y_portraits = 16
 
-    self.canvas = love.graphics.newCanvas( getScale()*self.scale_factor, 14*getScale()*self.scale_factor)
+    self.space_y_portrait_containers = 100
+    self.space_y = 19
+
+    self.canvas = love.graphics.newCanvas( getScale(), 18*getScale())
 end)
 
 -- draw: None -> None
@@ -41,13 +42,13 @@ function PartyStats.draw(self)
     for i = 1, (# self.entities) do
         -- Draw Stats
         local entity = self.entities[i]
-        self:drawEntityStats(entity, offset_y + self.offset_stat_bars)
+        self:drawEntityStats(entity, offset_y + self.offset_y_stat_bars)
 
         -- Draw Portraits
-        self.entity_portrait_background:draw(getRelativePosX(0), getRelativePosY(offset_y), getScale()*self.scale_factor, getScale()*self.scale_factor)
-        self.entities_portraits[i]:draw(getRelativePosX(self.space_x_portraits), getRelativePosY(offset_y + 0.7*self.space_y), getScale()*self.scale_factor, getScale()*self.scale_factor)
+        self.entity_portrait_background:draw(getRelativePosX(0), getRelativePosY(offset_y), getScale(), getScale())
+        self.entities_portraits[i]:draw(getRelativePosX(self.offset_x_portraits), getRelativePosY(offset_y + self.offset_y_portraits), getScale(), getScale())
 
-        offset_y = offset_y + 3*self.space_y + self.space_between_y
+        offset_y = offset_y + self.space_y_portrait_containers
     end
 end
 
@@ -66,21 +67,21 @@ function PartyStats.drawEntityStats(self, entity, offset_y)
     local width = hp*10
 
     love.graphics.setColor(self.hp_color)
-    love.graphics.draw(self.canvas, getRelativePosX(self.space_x), getRelativePosY(offset_y), 0, width, 1)
+    love.graphics.draw(self.canvas, getRelativePosX(self.offset_x_stat_bars), getRelativePosY(offset_y), 0, width, 1)
 
     -- Draw Mp
     offset_y = offset_y + self.space_y
     local width = mp*10
 
     love.graphics.setColor(self.mp_color)
-    love.graphics.draw(self.canvas, getRelativePosX(self.space_x), getRelativePosY(offset_y), 0, width, 1)
+    love.graphics.draw(self.canvas, getRelativePosX(self.offset_x_stat_bars), getRelativePosY(offset_y), 0, width, 1)
 
     -- Draw Guard
     offset_y = offset_y + self.space_y
     local width = guard*5
 
     love.graphics.setColor(self.guard_color)
-    love.graphics.draw(self.canvas, getRelativePosX(self.space_x), getRelativePosY(offset_y), 0, width, 1)
+    love.graphics.draw(self.canvas, getRelativePosX(self.offset_x_stat_bars), getRelativePosY(offset_y), 0, width, 1)
 
     love.graphics.setColor(1,1,1,1)
     love.graphics.setShader()
