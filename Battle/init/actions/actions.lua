8871b0b1c4e2dcedba379b8a97a09f3f6b00234d
Tomás Ahumada <tomyahu@gmail.com>
2020-02-11 16:41:21 -0300

    modified escape functions to accept unescapable battles

--- Battle/init/actions/actions.lua
+++ Battle/init/actions/actions.lua
@@ -30,13 +30,12 @@ action_build:setStartPiece(BATTLE_ACTION_PIECE_BORDER)
 action_build:setEndPiece(BATTLE_ACTION_PIECE_BORDER)
 action_build:setTarget(BATTLE_TARGET_SELF)
 action_build:setActionFunction( function(source_entity, target_entity)
+    local ctrl = application:getCurrentCtrl()
     local view = application:getCurrentView()
     local message_displayer = view:getMessageDisplayer()
 
-    if math.random() < 0.25 then
-        local ctrl = application:getCurrentCtrl()
-        ctrl:getTurnManager():setBattleOver(true)
-
+    if math.random() < 0.25 and ctrl:canEscape() then
+        ctrl:escape()
         message_displayer:displayMessage("Successfully ran away.", 1)
     else
         message_displayer:displayMessage("Couldn't escape.", 1)
@@ -108,10 +107,12 @@ action_build:setActionFunction( function(source_entity, target_entity)
     local message_displayer = view:getMessageDisplayer()
 
     local ctrl = application:getCurrentCtrl()
-    ctrl:getTurnManager():setBattleOver(true)
-
-    print("True Escape")
-    message_displayer:displayMessage("Successfully ran away.", 1)
+    if ctrl:canEscape() then
+        ctrl:escape()
+        message_displayer:displayMessage("Successfully ran away.", 1)
+    else
+        message_displayer:displayMessage("This battle is un-escapable.", 1)
+    end
 end)
 table.insert(actions, action_build:getAction())
 
