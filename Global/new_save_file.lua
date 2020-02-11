66f734db8edf0fa2ce1958f6361a5e4c32ea453d
Tomás Ahumada <tomyahu@gmail.com>
2020-02-11 17:24:23 -0300

    changed how inventory works in global context

--- Global/new_save_file.lua
+++ Global/new_save_file.lua
@@ -8,6 +8,17 @@ save_file["Overworld"]["Position"] = {}
 save_file["Overworld"]["Position"]["x"] = 300
 save_file["Overworld"]["Position"]["y"] = 300
 
+-- items
+save_file["Items"] = {}
+
+-- Sets the party inventory, id -> item amount
+save_file["Items"][1] = 1
+save_file["Items"][2] = 3
+save_file["Items"][3] = 1
+save_file["Items"][7] = 5
+
+
+-- Battle Encounter stuff
 save_file["Battle"] = {}
 save_file["Battle"]["PlayerPartyMetadata"] = {}
 
@@ -31,37 +42,27 @@ player3["meta"]["mp"] = 0
 
 save_file["Battle"]["PlayerPartyMetadata"][1] = player1
 save_file["Battle"]["PlayerPartyMetadata"][3] = player2
+save_file["Battle"]["CanEscape"] = true
 
 save_file["Battle"]["EnemyPartyMetadata"] = {}
-save_file["Battle"]["EnemyPartyMetadata"][1] = player3
+save_file["Battle"]["EnemyPartyMetadata"][1] = nil
 save_file["Battle"]["EnemyPartyMetadata"][2] = player3
-save_file["Battle"]["EnemyPartyMetadata"][3] = player3
+save_file["Battle"]["EnemyPartyMetadata"][3] = nil
 
 save_file["Battle"]["Environment"] = "debug_environment1"
+save_file["Battle"]["Rewards"] = {}
+save_file["Battle"]["Rewards"]["Items"] = {}
 
--- items
-save_file["Items"] = {}
+local reward_item_1 = {}
+reward_item_1["id"] = 7
+reward_item_1["count"] = 2
 
-local item_1 = {}
-item_1["id"] = 1
-item_1["count"] = 1
+local reward_item_2 = {}
+reward_item_2["id"] = 1
+reward_item_2["count"] = 1
 
-local item_2 = {}
-item_2["id"] = 2
-item_2["count"] = 3
-
-local item_3 = {}
-item_3["id"] = 3
-item_3["count"] = 1
-
-local item_4 = {}
-item_4["id"] = 7
-item_4["count"] = 5
-
-table.insert(save_file["Items"], item_1)
-table.insert(save_file["Items"], item_2)
-table.insert(save_file["Items"], item_3)
-table.insert(save_file["Items"], item_4)
+table.insert(save_file["Battle"]["Rewards"]["Items"], reward_item_1)
+table.insert(save_file["Battle"]["Rewards"]["Items"], reward_item_2)
 
 
 local function getNewSaveFile()
